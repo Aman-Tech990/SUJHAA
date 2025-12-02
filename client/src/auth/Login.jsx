e99c6f2b7f9f616429a73e25492573913674f4e7
Aman Parida <amantechz467@gmail.com>
2025-12-02 11:48:42 +0530

    Login via render

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://sujhaa-backend.onrender.com/api/auth/login",
         formData,
         {
           withCredentials: true,
